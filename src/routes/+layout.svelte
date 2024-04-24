ba8709d64cdfb17904c7e24aa45145be4038115b
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-04-24 16:11:43 +0100

    add title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script>
-  import "../app.css";
-  const {children} = $props()
+	import '../app.css';
+	const { children } = $props();
 </script>
+
+<svelte:head>
+	<title>Neko f**k</title>
+</svelte:head>
 
 {@render children()}
