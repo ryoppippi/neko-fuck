dcb1b08b90e8ad4a24c7fb0826547f8ade661c74
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-07-14 10:51:52 +0100

    feat(vite.config): add UnpluginTypia to Vite plugins
    This commit introduces UnpluginTypia to the Vite configuration. This
    plugin will enhance the development experience by providing additional
    typography utilities.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import Icons from 'unplugin-icons/vite';
+import UnpluginTypia from '@ryoppippi/unplugin-typia/vite';
 
 export default defineConfig({
 	plugins: [
+		UnpluginTypia(),
 		sveltekit(),
 		Icons({
 			compiler: 'svelte',
