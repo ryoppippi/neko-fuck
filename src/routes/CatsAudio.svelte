29378049a2e1e460c99aca889f575e41628ce417
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-12-31 00:49:52 +0900

    chore: update to the stable version of svelte5

--- src/routes/CatsAudio.svelte
+++ src/routes/CatsAudio.svelte
@@ -1,4 +1,4 @@
-<script context="module" lang="ts">
+<script module lang="ts">
 	import { ghostChar, isCatsBfMapKey, type CatsBfMapKey } from '$lib/types';
 	import { isBfChar, type BFChar } from '$lib/bf';
 
